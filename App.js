c07915d40bb7b1c0fec7e2bd6748175c63daa5fc
JonathanRemonte <jvremonte@up.edu.ph>
2025-06-25 01:54:50 +0800

    refactor: cleaned app.js and index.js files

--- App.js
+++ App.js
@@ -1,28 +1,6 @@
-// In App.js in a new project
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-function RootStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
-    </Stack.Navigator>
-  );
-}
+import RootStack from '@navigation/RootStack';
 
 export default function App() {
   return (
