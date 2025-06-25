5c3d745d16957bd08da9274a4782a2538bf96fdd
JonathanRemonte <jvremonte@up.edu.ph>
2025-06-26 01:51:40 +0800

    feat: replaced tab library builder for swipe tab switching feature

--- src/navigation/MaterialTabNav.js
+++ src/navigation/MaterialTabNav.js
@@ -1,61 +1,23 @@
 import React from 'react';
-import { Alert, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useNavigation } from '@react-navigation/native';
 
-import { supabase } from '@lib/supabase';
 import HomeScreen from '@screens/Home';
 import InviteScreen from '@screens/Invite';
 import TeamScreen from '@screens/Team';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
-export default function HeaderAndTabs() {
+export default function MaterialTabNav() {
   const insets = useSafeAreaInsets();
-  const navigation = useNavigation();
-
-  // Display modal prompt before logout
-  const confirmLogout = () => {
-    Alert.alert(
-      'Confirm Logout',
-      'Are you sure you want to log out?',
-      [
-        {
-          text: 'Cancel',
-          style: 'cancel',
-        },
-        {
-          text: 'Logout',
-          style: 'destructive',
-          onPress: async () => {
-            const { error } = await supabase.auth.signOut();
-            if (error) {
-              Alert.alert('Logout Failed', error.message);
-            } else {
-              navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
-            }
-          },
-        },
-      ],
-      { cancelable: true }
-    );
-  };
-
-  // Dynamically render logout header button
-  const renderLogoutButton = () => (
-    <TouchableOpacity onPress={confirmLogout} style={{ marginRight: 16 }}>
-      <Ionicons name="log-out-outline" size={24} color="#111827" />
-    </TouchableOpacity>
-  );
 
   return (
     <Tab.Navigator
+      swipeEnabled={true}
+      tabBarPosition="bottom"
       initialRouteName="Home"
       screenOptions={({ route }) => ({
-        headerShown: true,
-        headerRight: renderLogoutButton,
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
@@ -80,16 +42,9 @@ export default function HeaderAndTabs() {
           borderTopWidth: 0.3,
           borderTopColor: '#e5e7eb',
           height: 60 + insets.bottom,
-          paddingBottom: insets.bottom,
+          paddingBottom: -10 + insets.bottom,
           paddingTop: 8,
-        },
-        headerStyle: {
-          backgroundColor: 'white',
-        },
-        headerTintColor: '#76b5c5',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+        }
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} options={{ headerTitle: 'Dashboard' }} />
